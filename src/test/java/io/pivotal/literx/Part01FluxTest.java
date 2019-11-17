873fcf2e2edf92094ca849da857a5e0a56819e38
agusantoso <agusantoso@gmail.com>
2019-11-16 20:58:44 -0800

    Part 1

--- src/test/java/io/pivotal/literx/Part01FluxTest.java
+++ src/test/java/io/pivotal/literx/Part01FluxTest.java
@@ -22,6 +22,7 @@ public class Part01FluxTest {
 
 		StepVerifier.create(flux)
 				.verifyComplete();
+
 	}
 
 //========================================================================================
